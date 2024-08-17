53b0b9ad08c558b88d29d6e055d08fd560c9549e
Zettalynx <115348070+Zettalynx@users.noreply.github.com>
2024-08-17 17:59:48 +0700

    Update generate_profile_badge.py

--- generate_profile_badge.py
+++ generate_profile_badge.py
@@ -1,22 +1,19 @@
 import requests
+import matplotlib.pyplot as plt
 
 # Ambil data dari GitHub API
 username = "Zettalynx"
 url = f"https://api.github.com/users/{username}"
 response = requests.get(url)
-user_data = response.json()
+user = response.json()
 
-# Simulasi data untuk badge (contoh)
-followers = user_data['followers']
+# Simulasi data untuk badge
+followers = user.get('followers', 0)
 
-# Buat SVG badge
-svg_content = f"""
-<svg xmlns="http://www.w3.org/2000/svg" width="200" height="50">
-  <style>
-    .title {{ font: bold 20px sans-serif; fill: #333; }}
-  </style>
-  <text x="10" y="30" class="title">Followers: {followers}</text>
-</svg>
-"""
-with open('profile_badge.svg', 'w') as file:
-    file.write(svg_content)
+# Buat badge dengan tema gelap
+plt.style.use('dark_background')
+plt.figure(figsize=(10, 2))
+plt.text(0.5, 0.5, f'Followers: {followers}', color='lightgreen', fontsize=20, ha='center', va='center')
+plt.axis('off')
+plt.title('GitHub Profile Badge', fontsize=16)
+plt.savefig('profile_badge.svg')
